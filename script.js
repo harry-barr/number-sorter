4dbffda134988e416826a2f8abebf400437449df
Harry Barr <harrybarr530@gmail.com>
2024-08-14 17:05:50 +0100

    comments

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const sortBtn = document.querySelector(".sort");
 const sortInputArray = (event) => {
   event.preventDefault();
   const inputValues = [...document.querySelectorAll(".values-dropdown")].map(
-    (dropdown) => Number(dropdown.value)
+    (dropdown) => Number(dropdown.value) //use .map() to create an array of numbers from the input values
   );
   const sortedValues = inputValues.sort((a, b) => {
     return a - b;
-  });
-  updateUI(sortedValues);
+  }); // use sort method to sort the newly-formed array into ascending order
+  updateUI(sortedValues); // call updateUI to display sorted numbers in the output section
 };
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
-  });
+  }); // update the output display with sorted values
 };
 
 sortBtn.addEventListener("click", sortInputArray);
